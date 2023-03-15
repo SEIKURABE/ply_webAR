a8e29e9ad74eeafe26710e9926815ee55e0949f1
toshiro-m420 <toshiro-m420@users.noreply.github.com>
2023-03-15 08:43:09 +0000

    deploy: 309ef23ed611ab6811db7e268533b76abb315bc8

--- _next/static/cR7G79dAz9BHkMb5ChgOV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-fae2bc5fd2d68e42.js","static/chunks/284-8a17683377fe5a2d.js","static/chunks/pages/index-4ef2693c3fa16fc6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/sg5G7NHi7nd4yFjK-ZJO6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-fae2bc5fd2d68e42.js","static/chunks/81-a33314bb483175a0.js","static/chunks/pages/index-43e459db8b22db4e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
