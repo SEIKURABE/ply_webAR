d4a92d40f699ef75f4db539b136d194bef2e4f2b
toshiro mogi <toshiro_m420@icloud.com>
2023-03-15 16:54:11 +0900

    change: nextjsのURLのルートを変更

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: "/ply_webAR",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
