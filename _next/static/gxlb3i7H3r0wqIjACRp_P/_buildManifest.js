2d272978558830b58918c6eca48f94f17d286dee
toshiro-m420 <toshiro-m420@users.noreply.github.com>
2023-03-17 07:29:17 +0000

    deploy: 22f805446f70ad40a96fb921373565a34d92ef52

---
+++ _next/static/gxlb3i7H3r0wqIjACRp_P/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-fae2bc5fd2d68e42.js","static/chunks/379-727734bee03282f8.js","static/chunks/pages/index-23f18549c50b5643.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
