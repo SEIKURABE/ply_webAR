fa603891993740e868b5177db40f37ca3739e003
toshiro mogi <toshiro_m420@icloud.com>
2023-03-29 17:19:06 +0900

    add: scss周りの環境追加

--- pages/webAR.tsx
+++ pages/webAR.tsx
@@ -16,50 +16,79 @@ import {
 import { addUrlPrefix } from "../util/addUrlPrefix";
 
 export function DeviceOrientationControlsStory() {
-  const cameraRef = useRef([]);
-  const deviceOrientationRef = useRef(null);
-
-  console.log(cameraRef.current);
-
-  return (
-    <>
-      <PerspectiveCamera ref={cameraRef} />
-
-      <DeviceOrientationControls
-        ref={deviceOrientationRef}
-        // args={cameraRef.current}
-      />
-
-      <Box args={[100, 100, 100, 4, 4, 4]}>
-        <meshBasicMaterial wireframe />
-        <axesHelper args={[100]} />
-      </Box>
-    </>
-  );
+  return <></>;
 }
 
 const WebAR = () => {
   const canvasRef = useRef(null);
 
-  return (
+  const isIos = () => {
+    const ua = navigator.userAgent.toLowerCase();
+    return (
+      ua.indexOf("iphone") >= 0 ||
+      ua.indexOf("ipad") >= 0 ||
+      ua.indexOf("ipod") >= 0
+    );
+  };
+
+  const checkDeviceOrien = () => {
+    // Promiseで書く
+    return new Promise((resolve, reject) => {
+      // iOS以外（android）の場合には追加処理が必要ないのでresolveを返す
+      if (!isIos()) resolve("resolve");
+
+      const deviceOrienEvent = () => {
+        window.removeEventListener(
+          "deviceorientation",
+          deviceOrienEvent,
+          false
+        );
+        resolve("resolve");
+      };
+      // deviceorientationイベントで既にスマホの傾きが取れているか確認
+      window.addEventListener("deviceorientation", deviceOrienEvent, false);
+    });
+  };
+
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    checkDeviceOrien()
+      .then(() => {
+        // checkDeviceOrien関数のPromiseの結果が出てからThreeシーンの生成を行わせる
+        setReady(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return ready ? (
     <div className='page-index'>
-      <Canvas
-        ref={canvasRef}
-        className='canvas'
-        camera={{ position: [0, 0, 5] }}
-      >
+      <Canvas ref={canvasRef} className='canvas'>
+        <PerspectiveCamera />
+        <DeviceOrientationControls />
         <ambientLight intensity={1} />
 
         <axesHelper args={[1]} />
 
         <Suspense fallback={null}>
           <>
-            {/* <Preload all /> */}
-
-            <DeviceOrientationControlsStory />
+            <Box args={[100, 100, 100, 4, 4, 4]}>
+              <meshBasicMaterial wireframe />
+              <axesHelper args={[100]} />
+            </Box>
           </>
         </Suspense>
       </Canvas>
+
+      <div className='modal'>
+        このページでは端末の向きと方向を取得し ます。
+        次に表示されるポップアップに従って「許可」を選択してください。
+        <button type='button' className='btn'>
+          OK
+        </button>
+      </div>
 
       <style jsx>{`
         .page-index {
@@ -68,9 +97,32 @@ const WebAR = () => {
           background-color: #555;
           position: absolute;
         }
+
+        .modal {
+          max-width: 320px;
+          padding: 16px 32px;
+          background: #fff;
+          position: fixed;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          color: #000;
+
+          .btn {
+            width: 100%;
+            height: 40px;
+            background: #000 !important;
+            @include flex-align;
+            margin: 24px auto 0;
+            text-align: center;
+            color: #fff;
+            @include font-size(16px);
+            cursor: pointer;
+          }
+        }
       `}</style>
     </div>
-  );
+  ) : null;
 };
 
 export default WebAR;
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   plugins: {
-    autoprefixer: {},
+    "postcss-color-function": {},
+    "postcss-easing-gradients": {},
+    "postcss-flexbugs-fixes": {},
+    "postcss-preset-env": {
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      stage: 3,
+      features: {
+        "custom-properties": false,
+      },
+    },
   },
-}
+};
