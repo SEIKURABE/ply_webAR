2f3086fb28d2a0ca987bedbdcb54a407590ed80d
toshiro mogi <toshiro_m420@icloud.com>
2023-03-29 18:25:03 +0900

    change: DeviceOrientationControlsのios対応中

--- pages/webAR.tsx
+++ pages/webAR.tsx
@@ -21,6 +21,7 @@ export function DeviceOrientationControlsStory() {
 
 const WebAR = () => {
   const canvasRef = useRef(null);
+  const DeviceOrientationControlsRef = useRef(null);
   const [isShowModal, showModal] = useState(true);
 
   const isIos = () => {
@@ -49,50 +50,41 @@ const WebAR = () => {
       // deviceorientationイベントで既にスマホの傾きが取れているか確認
       window.addEventListener("deviceorientation", deviceOrienEvent, false);
 
-      const alertMessage =
-        "モーションセンサーの使用が拒否されました。\nこのページを楽しむには、デバイスモーションセンサーの使用を許可する必要があります。\nSafariのアプリを再起動して、モーションセンサーの使用（「動作と方向」へのアクセス）を許可をしてください。";
-      const deviceOrienModalButton = document.getElementById(
-        "device-orien-modal-button"
-      );
-
-      deviceOrienModalButton?.addEventListener("click", () => {
-        // ここからスマホの傾きを取得するためのリクエストをする処理
-        if (
-          window.DeviceMotionEvent &&
-          (window.DeviceMotionEvent as any).requestPermission &&
-          typeof (window.DeviceMotionEvent as any).requestPermission ===
-            "function"
-        ) {
-          (window.DeviceMotionEvent as any)
-            .requestPermission()
-            .then((res: any) => {});
-        }
-        if (
-          window.DeviceOrientationEvent &&
-          (window.DeviceOrientationEvent as any).requestPermission &&
-          typeof (DeviceOrientationEvent as any).requestPermission ===
-            "function"
-        ) {
-          (window.DeviceOrientationEvent as any)
-            .requestPermission()
-            .then((res: any) => {
-              console.log(res);
-              if (res === "granted") {
-                // 許可が選択されたらモーダルを非表示にする
-                deviceOrienModalButton.classList.add("is-hidden");
-
-                resolve("resolve");
-              } else {
-                // 拒否されたらアラートを表示
-                alert(alertMessage);
-                reject("resolve");
-              }
-            });
-        } else {
-          alert(alertMessage);
-          reject("resolve");
-        }
-      });
+      // ここからスマホの傾きを取得するためのリクエストをする処理
+      if (
+        window.DeviceMotionEvent &&
+        (window.DeviceMotionEvent as any).requestPermission &&
+        typeof (window.DeviceMotionEvent as any).requestPermission ===
+          "function"
+      ) {
+        (window.DeviceMotionEvent as any)
+          .requestPermission()
+          .then((res: any) => {});
+      }
+      if (
+        window.DeviceOrientationEvent &&
+        (window.DeviceOrientationEvent as any).requestPermission &&
+        typeof (window.DeviceOrientationEvent as any).requestPermission ===
+          "function"
+      ) {
+        (window.DeviceOrientationEvent as any)
+          .requestPermission()
+          .then((res: any) => {
+            console.log(res);
+            if (res === "granted") {
+              // 許可が選択されたらモーダルを非表示にする
+              showModal(false);
+              resolve("resolve");
+            } else {
+              // 拒否されたらアラートを表示
+              alert(alertMessage);
+              reject("resolve");
+            }
+          });
+      } else {
+        alert(alertMessage);
+        reject("resolve");
+      }
     });
   };
 
@@ -109,11 +101,21 @@ const WebAR = () => {
       });
   }, []);
 
-  return ready ? (
+  const alertMessage =
+    "モーションセンサーの使用が拒否されました。\nこのページを楽しむには、デバイスモーションセンサーの使用を許可する必要があります。\nSafariのアプリを再起動して、モーションセンサーの使用（「動作と方向」へのアクセス）を許可をしてください。";
+
+  const handleClickModalButton = () => {
+    console.log(
+      "DeviceOrientationControlsRef",
+      DeviceOrientationControlsRef.current
+    );
+  };
+
+  return (
     <div className='page-index'>
       <Canvas ref={canvasRef} className='canvas'>
         <PerspectiveCamera />
-        <DeviceOrientationControls />
+        <DeviceOrientationControls ref={DeviceOrientationControlsRef} />
         <ambientLight intensity={1} />
 
         <axesHelper args={[1]} />
@@ -127,15 +129,20 @@ const WebAR = () => {
           </>
         </Suspense>
       </Canvas>
-
-      <div className='modal'>
-        このページでは端末の向きと方向を取得し ます。
-        次に表示されるポップアップに従って「許可」を選択してください。
-        <button type='button' id='device-orien-modal-button' className='btn'>
-          OK
-        </button>
-      </div>
-
+      $
+      {isShowModal && (
+        <div className='modal'>
+          このページでは端末の向きと方向を取得し ます。
+          次に表示されるポップアップに従って「許可」を選択してください。
+          <button
+            type='button'
+            className='btn'
+            onClick={handleClickModalButton}
+          >
+            OK
+          </button>
+        </div>
+      )}
       <style jsx>{`
         .page-index {
           width: 100%;
@@ -146,6 +153,7 @@ const WebAR = () => {
 
         .modal {
           max-width: 320px;
+          width: 100%;
           padding: 16px 32px;
           background: #fff;
           position: fixed;
@@ -153,10 +161,6 @@ const WebAR = () => {
           left: 50%;
           transform: translate(-50%, -50%);
           color: #000;
-
-          &.is-hidden {
-            display: none;
-          }
 
           .btn {
             width: 100%;
@@ -172,7 +176,7 @@ const WebAR = () => {
         }
       `}</style>
     </div>
-  ) : null;
+  );
 };
 
 export default WebAR;
