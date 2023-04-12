542cba852e38713e64253127e4e6710029ee2d3a
toshiro mogi <toshiro_m420@icloud.com>
2023-04-12 20:53:18 +0900

    change: 確認用

--- pages/marker.jsx
+++ pages/marker.jsx
@@ -2,42 +2,65 @@ import React, { useEffect, useRef, useState, Suspense } from "react";
 import Script from "next/script";
 
 import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import {
+  Environment,
+  Box,
+  PerspectiveCamera,
+  DeviceOrientationControls,
+  OrbitControls,
+  Points,
+  Preload,
+} from "@react-three/drei";
+
 import { Entity, Scene } from "aframe-react";
 
 const WebAR = () => {
-  const canvasRef = useRef(null);
+  const sceneContainerRef = useRef(null);
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      require("aframe");
-      setMounted(true);
+      const load = async () => {
+        await require("aframe");
+
+        setMounted(true);
+      };
+
+      load();
     }
   });
 
-  const [detectorActivated, setDetectorActivated] = useState(false);
+  // const [detectorActivated, setDetectorActivated] = useState(false);
 
-  const handleDetectorActivate = () => {
-    alert("handleDetectorActivate !!!!");
-    setDetectorActivated(true);
-  };
+  // const handleDetectorActivate = () => {
+  //   alert("handleDetectorActivate !!!!");
+  //   setDetectorActivated(true);
+  // };
 
   return mounted ? (
     <div className='page-webar'>
-      <a-scene embedded arjs vr-mode-ui='enabled: false'>
-        <a-assets>
-          <a-asset-item
-            id='model'
-            src='./test/Chair.glb'
-            scale='1 1 1'
-          ></a-asset-item>
-        </a-assets>
+      {React.createElement(
+        "scene-container",
+        {
+          ref: sceneContainerRef,
+        },
+        <a-scene embedded arjs vr-mode-ui='enabled: false'>
+          <a-assets>
+            <a-asset-item
+              id='model'
+              src='test/Chair.glb'
+              scale='1 1 1'
+            ></a-asset-item>
+          </a-assets>
 
-        <a-marker type='pattern' url='./marker/pattern-marker.patt'>
-          <a-entity gltf-model='#model' rotation='-90 0 0'></a-entity>
-        </a-marker>
-        <a-entity camera></a-entity>
-      </a-scene>
+          <a-marker type='pattern' url='marker/pattern-marker.patt'>
+            <a-entity gltf-model='#model' rotation='-90 0 0'></a-entity>
+          </a-marker>
+
+          <a-entity camera></a-entity>
+        </a-scene>
+      )}
 
       <style jsx>{`
         .page-webar {
